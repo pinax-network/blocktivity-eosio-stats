a56b999e347214d292b05063ef4a150e37af9c4c
Denis <carriere.denis@gmail.com>
2019-11-06 21:09:56 -0500

    add history

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -66,6 +66,73 @@ module.exports = {
       },
       autorestart: true,
       log_date_format : "YYYY-MM-DD HH:mm"
+    },
+    {
+      name: "blocktivity-eos-history",
+      script: 'history.ts',
+      env: {
+        NODEOS_ENDPOINT: "http://eos.eosn.io",
+        HISTORY_TYPE: "dfuse",
+        NETWORK: "mainnet"
+      },
+      autorestart: true,
+      log_date_format : "YYYY-MM-DD HH:mm"
+    },
+    {
+      name: "blocktivity-jungle-history",
+      script: 'history.ts',
+      env: {
+        NODEOS_ENDPOINT: "http://jungle.eosn.io",
+        HISTORY_TYPE: "dfuse",
+        NETWORK: "jungle"
+      },
+      autorestart: true,
+      log_date_format : "YYYY-MM-DD HH:mm"
+    },
+    {
+      name: "blocktivity-kylin-history",
+      script: 'history.ts',
+      env: {
+        NODEOS_ENDPOINT: "http://kylin.eosn.io",
+        HISTORY_TYPE: "dfuse",
+        NETWORK: "kylin"
+      },
+      autorestart: true,
+      log_date_format : "YYYY-MM-DD HH:mm"
+    },
+    {
+      name: "blocktivity-bos-history",
+      script: 'history.ts',
+      env: {
+        NODEOS_ENDPOINT: "http://bos.eosn.io",
+        HISTORY_TYPE: "v1",
+        NODEOS_HISTORY_ENDPOINT: "http://api.bossweden.org"
+      },
+      autorestart: true,
+      log_date_format : "YYYY-MM-DD HH:mm"
+    },
+    {
+      name: "blocktivity-wax-history",
+      script: 'history.ts',
+      env: {
+        NODEOS_ENDPOINT: "http://wax.eosn.io",
+        HISTORY_TYPE: "dfuse",
+        NETWORK: "wax"
+      },
+      autorestart: true,
+      log_date_format : "YYYY-MM-DD HH:mm"
+    },
+    {
+      name: "blocktivity-meetone-history",
+      script: 'history.ts',
+      env: {
+        NODEOS_ENDPOINT: "http://meetone.eosn.io",
+        HISTORY_TYPE: "v1",
+        NODEOS_HISTORY_ENDPOINT: "https://fullnode.meet.one",
+        ACTOR: "blocktivit.m"
+      },
+      autorestart: true,
+      log_date_format : "YYYY-MM-DD HH:mm"
     }
   ]
 };
